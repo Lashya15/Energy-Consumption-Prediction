60193578e9767a8d0f2f121bfe7a726dd875f2e2
Lashya15 <lashyapinninti@gmail.com>
2026-05-13 23:01:31 +0530

    Fixed Render deployment

--- app.py
+++ app.py
@@ -277,5 +277,8 @@ def evaluate():
 
 
 # ================= RUN =================
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
